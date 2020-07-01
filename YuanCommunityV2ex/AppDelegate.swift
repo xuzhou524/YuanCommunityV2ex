82ace4e1d0c9b464565eb685123150e0461e0c04
xuzhou524 <xuuohz@163.com>
2020-07-01 14:52:18 +0800

    bak

--- YuanCommunityV2ex/AppDelegate.swift
+++ YuanCommunityV2ex/AppDelegate.swift
@@ -18,7 +18,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         URLProtocol.registerClass(XZWebViewImageProtocol.self)
         #if DEBUG
-        GADMobileAds.sharedInstance().start(completionHandler: nil)
         #else
         GADMobileAds.sharedInstance().start(completionHandler: nil)
         #endif
